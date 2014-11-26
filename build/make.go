b40df965cfdcb8f595df17efa9130cdd6557483f
Kashishm <kashishmunjal64@gmail.com>
2014-11-26 17:49:03 +0530

    changes in createZip : creating header when directory detected

--- build/make.go
+++ build/make.go
@@ -399,6 +399,7 @@ func createZip(dir, packageName string) {
 		}
 		infoHeader.Name = strings.Replace(path, fmt.Sprintf("%s%c", packageName, filepath.Separator), "", 1)
 		if info.IsDir() {
+			zipWriter.CreateHeader(infoHeader)
 			return nil
 		}
 		writer, err := zipWriter.CreateHeader(infoHeader)
