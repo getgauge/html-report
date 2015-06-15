4521a8cfc9a093f3aa3b0b6c68d7f02b2639017e
Prateek Baheti <prateektheone@gmail.com>
2015-06-15 14:28:36 +0530

    Updating proto messages

--- genproto.sh
+++ genproto.sh
@@ -24,5 +24,9 @@ PATH=$PATH:$GOPATH/bin protoc --go_out=../gauge_messages spec.proto
 PATH=$PATH:$GOPATH/bin protoc --go_out=../gauge_messages messages.proto
 
 cd ..
-sed  -i.backup '/import gauge_messages1 "spec.pb"/d' gauge_messages/messages.pb.go && sed  -i.backup 's/gauge_messages1.#g' gauge_messages/messages.pb.go && rm gauge_messages/messages.pb.go.backup
+sed  -i.backup '/import gauge_messages1 "spec.pb"/d' gauge_messages/messages.pb.go && sed  -i.backup 's/gauge_messages1.//g' gauge_messages/messages.pb.go && rm gauge_messages/messages.pb.go.backup
+sed  -i.backup '/import gauge_messages1 "spec.pb"/d' gauge_messages/api.pb.go && sed  -i.backup 's/gauge_messages1.//g' gauge_messages/api.pb.go && rm gauge_messages/api.pb.go.backup
+
+sed -i.backup '/import "."/d' gauge_messages/messages.pb.go && rm gauge_messages/messages.pb.go.backup
 go fmt github.com/getgauge/html-report/...
+
