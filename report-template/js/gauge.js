3d8eee89b792713fa1778933287fd2387dcda3ca
mahendrakariya <mahendrakariya@gmail.com>
2016-04-22 14:38:46 +0530

    Fixing colors indicating scenario results
    Fixes #52

--- report-template/js/gauge.js
+++ report-template/js/gauge.js
@@ -239,12 +239,6 @@ gaugeReport.controller('mainController', function($scope) {
         return undefined;
     };
 
-    $scope.getScenarioStatus = function(scenario) {
-        if (scenario.skipped)
-            return "skipped";
-        return scenario.failed;
-    };
-
     $scope.summaryItems = [{
         "key": "Environment",
         "value": $scope.result.environment
