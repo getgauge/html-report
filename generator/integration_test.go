d3df47e6e40522e17986030fd25bb1374a3268a1
mahendrakariya <mahendrakariya@gmail.com>
2016-07-14 09:40:01 +0530

    Add spec results to integration test
    Ref #86

--- generator/integration_test.go
+++ generator/integration_test.go
@@ -26,17 +26,67 @@ import (
 	"github.com/golang/protobuf/proto"
 )
 
+var passSpecRes1 = &gauge_messages.ProtoSpecResult{
+	Failed:        proto.Bool(false),
+	Skipped:       proto.Bool(false),
+	ExecutionTime: proto.Int64(211316),
+	ProtoSpec: &gauge_messages.ProtoSpec{
+		SpecHeading: proto.String("Passing Specification 1"),
+		Tags:        []string{"tag1", "tag2"},
+	},
+}
+
+var passSpecRes2 = &gauge_messages.ProtoSpecResult{
+	Failed:        proto.Bool(false),
+	Skipped:       proto.Bool(false),
+	ExecutionTime: proto.Int64(211316),
+	ProtoSpec: &gauge_messages.ProtoSpec{
+		SpecHeading: proto.String("Passing Specification 2"),
+		Tags:        []string{},
+	},
+}
+
+var passSpecRes3 = &gauge_messages.ProtoSpecResult{
+	Failed:        proto.Bool(false),
+	Skipped:       proto.Bool(false),
+	ExecutionTime: proto.Int64(211316),
+	ProtoSpec: &gauge_messages.ProtoSpec{
+		SpecHeading: proto.String("Passing Specification 3"),
+		Tags:        []string{"foo"},
+	},
+}
+
+var failSpecRes1 = &gauge_messages.ProtoSpecResult{
+	Failed:        proto.Bool(true),
+	Skipped:       proto.Bool(false),
+	ExecutionTime: proto.Int64(0),
+	ProtoSpec: &gauge_messages.ProtoSpec{
+		SpecHeading: proto.String("Failing Specification 1"),
+		Tags:        []string{},
+	},
+}
+
+var skipSpecRes1 = &gauge_messages.ProtoSpecResult{
+	Failed:        proto.Bool(false),
+	Skipped:       proto.Bool(true),
+	ExecutionTime: proto.Int64(0),
+	ProtoSpec: &gauge_messages.ProtoSpec{
+		SpecHeading: proto.String("Skipped Specification 1"),
+		Tags:        []string{"bar"},
+	},
+}
+
 var suiteRes = &gauge_messages.ProtoSuiteResult{
-	SpecResults:       make([]*gauge_messages.ProtoSpecResult, 0),
+	SpecResults:       []*gauge_messages.ProtoSpecResult{passSpecRes1, passSpecRes2, passSpecRes3, failSpecRes1, skipSpecRes1},
 	Failed:            proto.Bool(false),
-	SpecsFailedCount:  proto.Int32(0),
+	SpecsFailedCount:  proto.Int32(1),
 	ExecutionTime:     proto.Int64(122609),
-	SuccessRate:       proto.Float32(100),
+	SuccessRate:       proto.Float32(60),
 	Environment:       proto.String("default"),
 	Tags:              proto.String(""),
 	ProjectName:       proto.String("Gauge Project"),
 	Timestamp:         proto.String("Jul 13, 2016 at 11:49am"),
-	SpecsSkippedCount: proto.Int32(0),
+	SpecsSkippedCount: proto.Int32(1),
 }
 
 func TestHTMLGeneration(t *testing.T) {
