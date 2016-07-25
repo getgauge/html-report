6766a7f6bcfa0e1655455aee448ce7170d696d38
Kashishm <kashishmunjal64@gmail.com>
2016-07-25 16:11:52 +0530

    Integrate before and after suite hook failure occuring together | #86

--- generator/integration_test.go
+++ generator/integration_test.go
@@ -205,6 +205,28 @@ var suiteResWithAfterSuiteFailure = &gm.ProtoSuiteResult{
 	},
 }
 
+var suiteResWithBeforeAfterSuiteFailure = &gm.ProtoSuiteResult{
+	Failed:            proto.Bool(true),
+	SpecsFailedCount:  proto.Int32(0),
+	ExecutionTime:     proto.Int64(122609),
+	SuccessRate:       proto.Float32(0),
+	Environment:       proto.String("default"),
+	Tags:              proto.String(""),
+	ProjectName:       proto.String("Gauge Project"),
+	Timestamp:         proto.String("Jul 13, 2016 at 11:49am"),
+	SpecsSkippedCount: proto.Int32(0),
+	PreHookFailure: &gm.ProtoHookFailure{
+		ErrorMessage: proto.String("java.lang.RuntimeException"),
+		StackTrace:   proto.String(newStackTrace()),
+		ScreenShot:   []byte(newScreenshot()),
+	},
+	PostHookFailure: &gm.ProtoHookFailure{
+		ErrorMessage: proto.String("java.lang.RuntimeException"),
+		StackTrace:   proto.String(newStackTrace()),
+		ScreenShot:   []byte(newScreenshot()),
+	},
+}
+
 func TestHTMLGenerationHappyPath(t *testing.T) {
 	content, err := ioutil.ReadFile("_testdata/pass.html")
 	if err != nil {
@@ -247,6 +269,23 @@ func TestHTMLGenerationForAfterSuiteFailure(t *testing.T) {
 
 	buf := new(bytes.Buffer)
 	generate(suiteResWithAfterSuiteFailure, buf)
+
+	want := removeNewline(string(content))
+	got := removeNewline(buf.String())
+
+	if got != want {
+		t.Errorf("want:\n%q\ngot:\n%q\n", want, got)
+	}
+}
+
+func TestHTMLGenerationForBeforeAfterSuiteFailure(t *testing.T) {
+	content, err := ioutil.ReadFile("_testdata/before_after_suite_fail.html")
+	if err != nil {
+		t.Errorf("Error reading expected HTML file: %s", err.Error())
+	}
+
+	buf := new(bytes.Buffer)
+	generate(suiteResWithBeforeAfterSuiteFailure, buf)
 
 	want := removeNewline(string(content))
 	got := removeNewline(buf.String())
